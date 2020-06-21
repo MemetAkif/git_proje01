9592f3dde2778ba3451486f63a3fefeef7d4dee8
Mehmet <haciarifyildiz@gmail.com>
2020-06-21 20:48:36 +0200

    ilk commit local reporsitoryden

---
+++ src/packge01/FirstClass.java
@@ -0,0 +1,10 @@
+package packge01;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("ilk commit from local respository");
+	}
+
+}
